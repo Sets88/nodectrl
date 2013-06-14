0cd302b6daca7ca2a208ed4a1310e56fdfb6ebd1
Sets88 <sets88@mail.ru>
2013-06-14 09:28:09 +0400

    added method for hash ip with secret for api

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask import make_response
 from flask import request, redirect
 from flask import flash, url_for
-from hashlib import sha512
+from hashlib import sha512, sha1
 
 class Auth(object):
     def __init__(self, users, secret):
@@ -40,6 +40,18 @@ class Auth(object):
 
     def hash(self, password, user):
         return sha512(self.secret + sha512(password + user).hexdigest() + self.secret).hexdigest()
+
+    def simplehash(self, key):
+        return sha1(self.secret + sha512(key).hexdigest() + self.secret).hexdigest()
+
+    def check_ip_hash(self, hashh, ip):
+        if self.simplehash(ip) == hashh:
+            return True
+        else:
+            return False
+
+    def get_ip_hash(self, ip):
+        return self.simplehash(ip)
 
     def is_logged(self):
         if len(self.userlist) == 0:
