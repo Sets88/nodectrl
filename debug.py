b5ccb51ed215f681300779fb1f7ecd649fe70b1e
Sets88 <sets88@mail.ru>
2013-06-25 11:48:39 +0400

    removed remarked trash

--- debug.py
+++ debug.py
@@ -3,8 +3,5 @@
 from web import app
 
 if __name__ == "__main__":
-    # app.debug = True
-    # DEBUG_WITH_APTANA: True
     app.run(use_debugger=True, debug=True,
             use_reloader=True)
-    # app.run()
